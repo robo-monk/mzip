00ee646a7591a1bd33495992af1f677f2b5c327c
robo-monk <rrobomonk@gmail.com>
2022-07-16 20:54:07 +0300

    i think it works

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,17 @@ struct Node<T> {
     left: Option<Box<Node<T>>>,
 }
 
+impl From<Node<(u32, char)>> for Option<Box<Node<(u32, char)>>> {
+    fn from(node: Node<(u32, char)>) -> Self {
+        Some(Box::new(node))
+    }
+
+    
+}
 
 impl Tree<(u32, char)> {
-    fn new() -> Self {
-        Tree { root: None }
+    fn new(root: Node<(u32, char)>) -> Self {
+        Tree { root: root.into() }
     }
 }
 
@@ -32,12 +39,25 @@ impl Node<(u32, char)> {
         Node {
             v,
             right: None,
-            left: None
+            left: None,
+        }
+    }
+
+    pub fn is_leaf(&self) -> bool {
+        self.right.is_none() && self.left.is_none()
+    }
+
+    fn serialize_debug(&self) {
+        if self.is_leaf() {
+            println!("[{} '{}']", self.v.0, self.v.1);
+        } else {
+            self.right.as_ref().unwrap().serialize_debug();
+            self.left.as_ref().unwrap().serialize_debug();
         }
     }
 
     pub fn value(&self) -> char {
-        return self.v.1
+        return self.v.1;
     }
     pub fn frequency(&self) -> u32 {
         return self.v.0;
@@ -61,30 +81,47 @@ impl Node<(u32, char)> {
         1. Calculate the frequency of each character within a string
         -> [2 t] [1 e] [1 s]
         2. Sort in increasing order of frequency
-        -> [1 e] [1 s] [2 t] (queue) 
+        -> [1 e] [1 s] [2 t] (queue)
+        3. Make each unique character a leaf node
         3. Create a new node Z. Left child of Z will be the minimum frequency, Right will be the 2nd min.
            Value of Z will be the sum of the 2 childs
-        -> 2 'z'[left: [1 e] right:[1 s]] [2 t] 
+        -> 2 'z'[left: [1 e] right:[1 s]] [2 t]
         4. Insert Z into the tree
 
 */
 
-fn queue_to_tree(map: HashMap<char, u32>) -> Tree<(u32, char)> {
+fn queue_to_tree(mut queue: Vec<Node<(u32, char)>>) -> Tree<(u32, char)> {
+    if queue.len() == 1 {
+        return Tree::new(queue.pop().unwrap());
+    }
 
-    let mut queue: Vec<(u32, Node<(u32, char)>)> = Vec::new();
+    queue.sort_by(|a, b| b.frequency().cmp(&a.frequency()));
 
+    let left = queue.pop().unwrap();
+    let right = queue.pop().unwrap();
+    let combined_frequency =  left.frequency() + right.frequency();
+    let new_node: Node<(u32, char)> = Node {
+        v: (combined_frequency, char::from(1)), // refactor so char could be none
+        right: right.into(),
+        left: left.into()
+     };
+
+    queue.push(new_node);
+
+    return queue_to_tree(queue);
+}
+
+// converts a HashMap of (char, frequency) to a Vec of (freq, Node)
+fn map_to_queue(map: HashMap<char, u32>) -> Vec<Node<(u32, char)>> {
+    // let mut queue: Vec<(u32, Node<(u32, char)>)> = Vec::new();
+    let mut queue: Vec<Node<(u32, char)>> = Vec::new();
+    // let mut queue: Vec<(char, u32)> = map.values().into();
     for (c, frequency) in map.iter() {
         println!("char: {c} freq: {frequency}");
-        let node = Node::new((*frequency, *c));
-        queue.push((*frequency, node));
+        queue.push(Node::new((*frequency, *c)));
     }
 
-    queue.sort_by(|a, b| a.0.cmp(&b.0));
-
-    for t in queue.iter() {
-        println!("[({}) '{}']", t.1.frequency(), t.1.value());
-    }
-    return Tree::new();
+    queue
 }
 
 fn encode(content: String) -> String {
@@ -104,7 +141,10 @@ fn encode(content: String) -> String {
         // Node::new((frequency, c));
     }
 
-    let tree = queue_to_tree(map);
+    let queue = map_to_queue(map);
+    let tree = queue_to_tree(queue);
+
+    tree.root.unwrap().serialize_debug();
 
     return String::from("");
 }
